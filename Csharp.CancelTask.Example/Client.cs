9c09d3c60799e19bc1939b72a1d0b7e9032d8044
石井 健太 <i_m_a_walker@live.jp>
2022-07-17 17:11:26 +0900

    通常の実装パターンを書いてみた

---
+++ Csharp.CancelTask.Example/Client.cs
@@ -0,0 +1,24 @@
+﻿namespace Csharp.CancelTask.Example;
+
+public class Client
+{
+    // 基本的に非同期処理を実行するクラスでは、以下のように CancellationToken を伝搬させる。
+    // 最終的に非同期処理を実行する機能まで伝搬させてからキャンセル処理を実装する。
+    // このパターンが C# における Task のキャンセル処理である。
+    //
+    // アプリケーションに近い層だとこの引数はデフォルト値を外したほうがいい。
+    // なぜなら必ずキャンセル処理を定義することで実装漏れをふせぐことができるから。
+    // ↑これは考えつかなかったので今後は取り入れたい。
+    public async Task SendAsync(CancellationToken cancellationToken = default)
+    {
+        await SendAsyncCore(cancellationToken);
+    }
+
+    private async Task SendAsyncCore(CancellationToken cancellationToken)
+    {
+        foreach (var minutes in Enumerable.Range(1, 10).ToArray())
+        {
+            await Task.Delay(TimeSpan.FromSeconds(minutes), cancellationToken);
+        }
+    }
+}
--- Csharp.CancelTask.Example/Program.cs
+++ Csharp.CancelTask.Example/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Csharp.CancelTask.Example;
+
+var client = new Client();
+;
+// CancellationTokenSource は using することで Dispose したときに内部タイマーが停止するのでリークすることはない。
+// cf. https://github.com/dotnet/runtime/blob/215b39abf947da7a40b0cb137eab4bceb24ad3e3/src/libraries/System.Private.CoreLib/src/System/Threading/CancellationTokenSource.cs#L445
+using var cts = new CancellationTokenSource();
+cts.CancelAfter(TimeSpan.FromSeconds(5));           // この方法は「キャンセルできる」であって呼び出し側からすると「使いやすいとは言えない」コードになっている。
+
+// 上記の実装だと以下のコードで5秒経過すると System.Threading.Tasks.TaskCanceledException がスローされる。
+await client.SendAsync(cts.Token);
+
 Console.WriteLine("Hello, World!");
