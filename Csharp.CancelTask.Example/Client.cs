c484ae4ae68746677fffb04af2a99087ef55d91c
石井 健太 <i_m_a_walker@live.jp>
2022-07-17 17:54:14 +0900

    微調整

--- Csharp.CancelTask.Example/Client.cs
+++ Csharp.CancelTask.Example/Client.cs
@@ -1,12 +1,17 @@
 ﻿namespace Csharp.CancelTask.Example;
 
-public class Client
+public class Client : IDisposable
 {
+    // Dispose されたときにこのクラスが呼び出している非同期処理をキャンセルするための CancellationTokenSource。
+    // Dispose 呼び出された＝リクエストも終了させる 場合に使用する。
+    readonly CancellationTokenSource _clientLifetimeTokenSource;
+
     private readonly TimeSpan _timeout;
     
     public Client(TimeSpan timeout)
     {
         _timeout = timeout;
+        _clientLifetimeTokenSource = new CancellationTokenSource();
     }
 
     // 基本的に非同期処理を実行するクラスでは、以下のように CancellationToken を伝搬させる。
@@ -22,7 +27,7 @@ public class Client
         // なので、このクラスの呼出側から設定するのは面倒くさい。何度も呼び出すことになるので。
         // それを解決するのが CancellationTokenSource.CreateLinkedToTokenSource になる。
         // 引数の cancellationToken を連結した CancellationTokenSource を作り出す。
-        using var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+        using var cts = CancellationTokenSource.CreateLinkedTokenSource(_clientLifetimeTokenSource.Token, cancellationToken);
         cts.CancelAfter(_timeout);
 
         try
@@ -31,12 +36,24 @@ public class Client
         }
         catch (OperationCanceledException ex) when (ex.CancellationToken == cts.Token)
         {
+            // when (ex.CancellationToken == cts.Token) とすることで .SendAsync で発生したキャンセルの原因の判別が可能になる。
+            // しかし、内部的に CancellationTokenSource.CreateLinkedToTokenSource を使って、かつ例外処理を実装していないとこの判別が不可能になる。
+            // ex. https://github.com/dotnet/runtime/issues/21965
+            // なので、上記のような内部で連結するような処理を実装した場合は例外処理もセットで実装する必要がある。
+
             if (cancellationToken.IsCancellationRequested)
             {
                 // 呼び出し元の CancellationToken が原因でキャンセルされた場合はこっちのルートを通る。
                 // OperationCanceledException の第3引数に、呼び出し元の CancellationToken を渡せば
                 // 呼び出し元側で指定したタイムアウトによるものかどうかが判別できる。
                 throw new OperationCanceledException(ex.Message, ex, cancellationToken);
+            }
+            else if (_clientLifetimeTokenSource.IsCancellationRequested)
+            {
+                // クライアント自体が Dispose されたときに呼び出される。
+                // OperationCanceledException で呼び出し元が判別できるようにするか、
+                // 独自の例外で判別できるようにするかはアプリケーションの設計次第。
+                throw new OperationCanceledException("Client is disposed.", ex, _clientLifetimeTokenSource.Token);
             }
             else
             {
@@ -53,5 +70,11 @@ public class Client
         {
             await Task.Delay(TimeSpan.FromSeconds(minutes), cancellationToken);
         }
+    }
+
+    public void Dispose()
+    {
+        _clientLifetimeTokenSource.Cancel();
+        _clientLifetimeTokenSource.Dispose();
     }
 }
--- Csharp.CancelTask.Example/Program.cs
+++ Csharp.CancelTask.Example/Program.cs
@@ -10,19 +10,19 @@ var client = new Client(TimeSpan.FromSeconds(5));
 // CancellationTokenSource は using することで Dispose したときに内部タイマーが停止するのでリークすることはない。
 // cf. https://github.com/dotnet/runtime/blob/215b39abf947da7a40b0cb137eab4bceb24ad3e3/src/libraries/System.Private.CoreLib/src/System/Threading/CancellationTokenSource.cs#L445
 using var cts = new CancellationTokenSource();
+
 try
 {
     // 上記の実装だと以下のコードで5秒経過すると System.Threading.Tasks.TaskCanceledException がスローされる。
     await client.SendAsync(cts.Token);
+
+    // 上記を別メソッドにして以下のように Cancel 呼び出すと、OperationCanceledException がスローされる。
+    // 呼び出し元として使う場合はこっちがメインになるはず。
+    //cts.Cancel();
 }
-catch (OperationCanceledException ex)
-        when (ex.CancellationToken == cts.Token)
+catch (TimeoutException ex)
 {
     // タイムアウト発生後はこっちのルートへ。
-    // when (ex.CancellationToken == cts.Token) とすることで .SendAsync で発生したキャンセルの原因の判別が可能になる。
-    // しかし、内部的に CancellationTokenSource.CreateLinkedToTokenSource を使って、かつ例外処理を実装していないとこの判別が不可能になる。
-    // ex. https://github.com/dotnet/runtime/issues/21965
-    // なので、上記のような内部で連結するような処理を実装した場合は例外処理もセットで実装する必要がある。
     Console.WriteLine(ex);
 }
 
