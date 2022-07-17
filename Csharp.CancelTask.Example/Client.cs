56f2fa83f2cccb819d1755e6d886e277969495f4
石井 健太 <i_m_a_walker@live.jp>
2022-07-17 17:25:47 +0900

    CancellationTokenSoruce のタイムアウト処理を内部で実装した。

--- Csharp.CancelTask.Example/Client.cs
+++ Csharp.CancelTask.Example/Client.cs
@@ -2,6 +2,13 @@
 
 public class Client
 {
+    private readonly TimeSpan _timeout;
+    
+    public Client(TimeSpan timeout)
+    {
+        _timeout = timeout;
+    }
+
     // 基本的に非同期処理を実行するクラスでは、以下のように CancellationToken を伝搬させる。
     // 最終的に非同期処理を実行する機能まで伝搬させてからキャンセル処理を実装する。
     // このパターンが C# における Task のキャンセル処理である。
@@ -11,10 +18,17 @@ public class Client
     // ↑これは考えつかなかったので今後は取り入れたい。
     public async Task SendAsync(CancellationToken cancellationToken = default)
     {
-        await SendAsyncCore(cancellationToken);
+        // タイムアウトの時間設定はたいていアプリケーションで固定になる。
+        // なので、このクラスの呼出側から設定するのは面倒くさい。何度も呼び出すことになるので。
+        // それを解決するのが CancellationTokenSource.CreateLinkedToTokenSource になる。
+        // 引数の cancellationToken を連結した CancellationTokenSource を作り出す。
+        using var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+        cts.CancelAfter(_timeout);
+
+        await SendCoreAsync(cts.Token);
     }
 
-    private async Task SendAsyncCore(CancellationToken cancellationToken)
+    private async Task SendCoreAsync(CancellationToken cancellationToken)
     {
         foreach (var minutes in Enumerable.Range(1, 10).ToArray())
         {
--- Csharp.CancelTask.Example/Program.cs
+++ Csharp.CancelTask.Example/Program.cs
@@ -2,14 +2,28 @@
 
 using Csharp.CancelTask.Example;
 
-var client = new Client();
-;
+// 引数でタイムアウトを指定することで実装が楽になる。
+// もしくはデフォルトコンストラクタでタイムアウトを実装できるようにして、
+// 引数ありコンストラクタでタイムアウトを設定できるようにするのもありかも。
+var client = new Client(TimeSpan.FromSeconds(5));
+
 // CancellationTokenSource は using することで Dispose したときに内部タイマーが停止するのでリークすることはない。
 // cf. https://github.com/dotnet/runtime/blob/215b39abf947da7a40b0cb137eab4bceb24ad3e3/src/libraries/System.Private.CoreLib/src/System/Threading/CancellationTokenSource.cs#L445
 using var cts = new CancellationTokenSource();
-cts.CancelAfter(TimeSpan.FromSeconds(5));           // この方法は「キャンセルできる」であって呼び出し側からすると「使いやすいとは言えない」コードになっている。
-
-// 上記の実装だと以下のコードで5秒経過すると System.Threading.Tasks.TaskCanceledException がスローされる。
-await client.SendAsync(cts.Token);
+try
+{
+    // 上記の実装だと以下のコードで5秒経過すると System.Threading.Tasks.TaskCanceledException がスローされる。
+    await client.SendAsync(cts.Token);
+}
+catch (OperationCanceledException ex)
+        when (ex.CancellationToken == cts.Token)
+{
+    // タイムアウト発生後はこっちのルートへ。
+    // when (ex.CancellationToken == cts.Token) とすることで .SendAsync で発生したキャンセルの原因の判別が可能になる。
+    // しかし、内部的に CancellationTokenSource.CreateLinkedToTokenSource を使って、かつ例外処理を実装していないとこの判別が不可能になる。
+    // ex. https://github.com/dotnet/runtime/issues/21965
+    // なので、上記のような内部で連結するような処理を実装した場合は例外処理もセットで実装する必要がある。
+    Console.WriteLine(ex);
+}
 
 Console.WriteLine("Hello, World!");
